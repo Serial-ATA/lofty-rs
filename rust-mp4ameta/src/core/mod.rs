d9fd42f4910bdf79d02339d9c953608a363db2a6
Tianyi <ShiTianyi2001@outlook.com>
2020-10-25 15:44:45 +0000

    use mp4ameta = 0.5.1

--- rust-mp4ameta/src/core/mod.rs
+++
@@ -1,7 +0,0 @@
-/// Contains constants, structs and functions for working with MPEG-4 metadata atoms.
-pub mod atom;
-pub mod content;
-/// Contains cosntants, structs and functions for working with data held inside MPEG-4 metadata atoms.
-pub mod data;
-/// Contains structs and constants for working with types held inside data atoms.
-pub mod types;
