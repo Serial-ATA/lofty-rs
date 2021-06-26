1448d8cca9f90fc3988cda8dc5e3cd6715c17544
Serial <69764315+Serial-ATA@users.noreply.github.com>
2021-06-26 15:06:39 -0400

    Fix modules missing feature conditions
    Signed-off-by: Serial <69764315+Serial-ATA@users.noreply.github.com>

--- src/components/logic/mod.rs
+++ src/components/logic/mod.rs
@@ -1,3 +1,19 @@
-mod constants;
-pub(crate) mod read;
-pub(crate) mod write;
+#[cfg(any(feature = "format-opus", feature = "format-vorbis", feature = "format-riff"))]
+pub(crate) mod constants;
+
+#[cfg(any(feature = "format-opus", feature = "format-vorbis"))]
+mod crc;
+#[cfg(any(feature = "format-opus", feature = "format-vorbis"))]
+pub(crate) mod ogg_generic;
+
+#[cfg(feature = "format-opus")]
+pub(crate) mod opus;
+
+#[cfg(feature = "format-vorbis")]
+pub(crate) mod vorbis;
+
+#[cfg(feature = "format-flac")]
+pub(crate) mod flac;
+
+#[cfg(feature = "format-riff")]
+pub(crate) mod riff;
