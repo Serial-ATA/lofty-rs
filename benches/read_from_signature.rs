886a9f37cec866afbbc7427db4ea5f4559e4342d
Serial <69764315+Serial-ATA@users.noreply.github.com>
2021-06-27 14:25:29 -0400

    Fix benches
    Signed-off-by: Serial <69764315+Serial-ATA@users.noreply.github.com>

--- benches/read_from_signature.rs
+++ benches/read_from_signature.rs
@@ -13,9 +13,9 @@ test_read!(read_ape, "tests/assets/a.ape");
 test_read!(read_flac, "tests/assets/a.flac");
 test_read!(read_m4a, "tests/assets/a.m4a");
 test_read!(read_mp3, "tests/assets/a.mp3");
-// test_read!(read_ogg, "tests/assets/a.ogg");
+test_read!(read_vorbis, "tests/assets/a.ogg");
 test_read!(read_opus, "tests/assets/a.opus");
-test_read!(read_wav, "tests/assets/a.wav");
+test_read!(read_riff, "tests/assets/a.wav");
 
 fn bench_sig(c: &mut Criterion) {
 	let mut g = c.benchmark_group("From signature");
@@ -23,9 +23,9 @@ fn bench_sig(c: &mut Criterion) {
 	g.bench_function("FLAC", |b| b.iter(read_flac));
 	g.bench_function("MP4", |b| b.iter(read_m4a));
 	g.bench_function("MP3", |b| b.iter(read_mp3));
-	// g.bench_function("OGG", |b| b.iter(read_ogg));
+	g.bench_function("VORBIS", |b| b.iter(read_vorbis));
 	g.bench_function("OPUS", |b| b.iter(read_opus));
-	g.bench_function("WAV", |b| b.iter(read_wav));
+	g.bench_function("RIFF", |b| b.iter(read_riff));
 }
 
 criterion_group!(benches, bench_sig);
