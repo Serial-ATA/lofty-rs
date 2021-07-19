f7fa066698250d2afedc5d285c3f5fe9af68f405
Serial <squared@disroot.org>
2021-07-19 13:13:44 -0400

    Add a FileProperties struct for future use

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,2 +1,11 @@
 pub(crate) mod logic;
 pub(crate) mod tags;
+
+use std::time::Duration;
+
+pub struct FileProperties {
+    duration: Duration,
+    bitrate: u32,
+    sample_rate: u32,
+    channels: u8,
+}
