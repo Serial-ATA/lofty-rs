4d4341feafc07868eb6455bcb7c29976efd9d8bf
Serial <69764315+Serial-ATA@users.noreply.github.com>
2021-08-19 15:07:40 -0400

    Correct Probe methods

--- src/probe.rs
+++ src/probe.rs
@@ -1,85 +1,77 @@
-#[allow(clippy::wildcard_imports)]
-use crate::components::tags::*;
-use crate::{AudioTag, FileType, LoftyError, Result, Tag, TaggedFile};
+use crate::files::*;
+use crate::types::file::AudioFile;
+use crate::{FileType, LoftyError, Result, TaggedFile};
 
-use std::fs::{File, OpenOptions};
-use std::io::{Cursor, Read, Seek, SeekFrom};
+use std::io::{Cursor, Read, Seek};
 use std::path::Path;
 
-use byteorder::ReadBytesExt;
-use std::convert::TryInto;
-use crate::components::logic::ogg::VorbisFile;
-use crate::components::logic::iff::RiffFile;
-
-/// Provides a way to extract a [`FileType`] from a reader
-#[derive(Default)]
-pub struct Probe(());
+/// Provides a way to extract a [`FileType`] or [`TaggedFile`] from a reader
+pub struct Probe;
 
 impl Probe {
 	/// Create a new `Probe`
 	pub fn new() -> Self {
-		Self::default()
+		Self
+	}
+
+	/// Attempts to get the [`FileType`] based on the data in the reader
+	pub fn file_type<R>(&self, reader: &mut R) -> Option<FileType>
+	where
+		R: Read + Seek,
+	{
+		FileType::try_from_sig(reader).ok()
+	}
+
+	/// Attempts to get a [`FileType`] from a path
+	///
+	/// NOTE: This is based on the content of the file.
+	/// If you want to guess based on extension, see [`Probe::file_type_from_extension`](Self::file_type_from_extension)
+	pub fn file_type_from_path(&self, path: impl AsRef<Path>) -> Option<FileType> {
+		if let Ok(content) = std::fs::read(&path) {
+			let mut cursor = Cursor::new(content);
+			return self.file_type(&mut cursor);
+		}
+
+		None
 	}
 
 	/// Attempts to get the [`FileType`] based on the file extension
 	///
 	/// NOTE: Since this only looks at the extension, the result could be incorrect.
+	pub fn file_type_from_extension(&self, path: impl AsRef<Path>) -> Option<FileType> {
+		if let Some(ext_os) = path.as_ref().extension() {
+			if let Some(ext) = ext_os.to_str() {
+				return FileType::try_from_ext(ext).ok();
+			}
+		}
+
+		None
+	}
+
+	/// Attempts to extract a [`TaggedFile`] from a reader
 	///
 	/// # Errors
 	///
-	/// * `path` does not exist
-	/// * `path` has no extension
-	/// * `path` has an unsupported/unknown extension
-	///
-	/// # Warning
-	/// Using this on a `WAV`/`AIFF`/`MP3` file will **always** assume there's an ID3 tag.
-	/// [`guess_from_content`](Probe::guess_from_content) is recommended in the event that other tags are present.
-	pub fn guess_from_extension(&self, path: impl AsRef<Path>) -> Result<TaggedFile> {
-		let mut c = Cursor::new(std::fs::read(&path)?);
-
-		let extension = path
-			.as_ref()
-			.extension()
-			.ok_or(LoftyError::UnknownFileExtension)?;
-
-		let extension_str = extension.to_str().ok_or(LoftyError::UnknownFileExtension)?;
-
-		FileType::try_from_ext(extension_str)?;
-
-		_read_from(&mut c, tag_type)
+	/// * The format couldn't be determined
+	pub fn read_from<R>(self, reader: &mut R) -> Result<TaggedFile>
+	where
+		R: Read + Seek,
+	{
+		match FileType::try_from_sig(reader) {
+			Ok(f_type) => _read_from(reader, f_type),
+			Err(_) => Err(LoftyError::UnknownFormat),
+		}
 	}
 
-	/// Attempts to get the tag format based on the file signature
-	///
-	/// NOTE: This is *slightly* slower than reading from extension, but more accurate.
-	/// The only times were this would really be necessary is if the file format being read
-	/// supports more than one metadata format (ex. RIFF), or there is no file extension.
+	/// Attempts to extract a [`TaggedFile`] from a path
 	///
 	/// # Errors
 	///
 	/// * `path` does not exist
 	/// * The format couldn't be determined
-	pub fn guess_from_content(&self, path: impl AsRef<Path>) -> Result<TaggedFile> {
-		let mut c = Cursor::new(std::fs::read(&path)?);
-		let file_type = FileType::try_from_sig(&mut c)?;
-
-		_read_from(&mut c, file_type)
-	}
-
-	/// Attempts to get the tag format based on the data in the reader
-	///
-	/// See [`guess_from_content`][Probe::guess_from_content] for important notes, errors, and warnings.
-	///
-	/// # Errors
-	///
-	/// Same as [`guess_from_content`][Probe::guess_from_content]
-	pub fn guess_from<R>(&self, reader: &mut R) -> Result<TaggedFile>
-	where
-		R: Read + Seek,
-	{
-		let file_type = FileType::try_from_sig(reader)?;
-
-		_read_from(reader, tag_type)
+	pub fn read_from_path(self, path: impl AsRef<Path>) -> Result<TaggedFile> {
+		let mut cursor = Cursor::new(std::fs::read(&path)?);
+		self.read_from(&mut cursor)
 	}
 }
 
@@ -88,13 +80,13 @@ where
 	R: Read + Seek,
 {
 	match file_type {
-		FileType::AIFF => AiffFile::read_from(),
-		FileType::APE => ApeFile::read_from(),
-		FileType::FLAC => FlacFile::read_from(),
-		FileType::MP3 => MpegFile::read_from(),
-		FileType::MP4 => // TODO,
-		FileType::Opus => OpusFile::read_from(),
-		FileType::Vorbis => VorbisFile::read_from(),
-		FileType::WAV => RiffFile::read_from(),
+		FileType::AIFF => Ok(AiffFile::read_from(reader)?.into()),
+		FileType::APE => Ok(ApeFile::read_from(reader)?.into()),
+		FileType::FLAC => Ok(FlacFile::read_from(reader)?.into()),
+		FileType::MP3 => Ok(MpegFile::read_from(reader)?.into()),
+		FileType::Opus => Ok(OpusFile::read_from(reader)?.into()),
+		FileType::Vorbis => Ok(VorbisFile::read_from(reader)?.into()),
+		FileType::WAV => Ok(WavFile::read_from(reader)?.into()),
+		_ => Err(LoftyError::UnknownFormat), // FileType::MP4 => {}, TODO,
 	}
 }
