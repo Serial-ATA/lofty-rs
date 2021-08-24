1d6fdb36007de2e7892d928f8951ad53ba6d3395
Serial <69764315+Serial-ATA@users.noreply.github.com>
2021-08-23 23:23:29 -0400

    Add Id3v2Frame enum, remove old files

--- src/logic/id3/v2/mod.rs
+++ src/logic/id3/v2/mod.rs
@@ -1,14 +1,15 @@
 use crate::logic::id3::decode_u32;
+use crate::logic::id3::v2::util::encapsulated_object::GEOBInformation;
+use crate::logic::id3::v2::util::sync_text::SyncTextInformation;
+use crate::types::picture::TextEncoding;
 use crate::Result;
 
 use std::io::{Read, Seek, SeekFrom};
 
 use byteorder::{BigEndian, ByteOrder};
 
+mod frame;
 pub(crate) mod util;
-mod v2;
-mod v3;
-mod v4;
 
 #[derive(PartialEq, Debug, Clone)]
 /// The ID3v2 version
@@ -19,6 +20,37 @@ pub enum Id3v2Version {
 	V3,
 	/// ID3v2.4
 	V4,
+}
+
+#[derive(PartialEq)]
+/// Different types of ID3v2 frames that require varying amounts of information
+pub enum Id3v2Frame {
+	/// Represents a "T..." (excluding TXXX) frame
+	Text(TextEncoding),
+	/// Represents a "TXXX" frame
+	///
+	/// This can be thought of as TXXX(encoding, description), as TXXX frames are often identified by descriptions.
+	UserText(TextEncoding, String),
+	/// Represents a "W..." (excluding WXXX) frame
+	///
+	/// Nothing needs to be provided as all URLs are [`TextEncoding::Latin1`]
+	URL,
+	/// Represents a "WXXX" frame
+	///
+	/// This can be thought of as WXXX(encoding, description), as WXXX frames are often identified by descriptions.
+	UserURL(TextEncoding, String),
+	/// Represents a "SYLT" frame
+	///
+	/// Due to the amount of information needed, it is contained in a separate struct, [`SyncTextInformation`]
+	SyncText(SyncTextInformation),
+	/// Represents a "GEOB" frame
+	///
+	/// Due to the amount of information needed, it is contained in a separate struct, [`GEOBInformation`]
+	EncapsulatedObject(GEOBInformation),
+	/// When an ID3v2.2 key couldn't be upgraded
+	///
+	/// This **will not** be written. It is up to the user to upgrade and store the key as [`ItemKey::Unknown`](crate::ItemKey::Unknown).
+	Outdated(String),
 }
 
 pub(crate) fn find_id3v2<R>(data: &mut R, read: bool) -> Result<Option<Vec<u8>>>
