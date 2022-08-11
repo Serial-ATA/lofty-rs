d251a589a4a987a796d875f6ddc9c995d3d1a8f3
sagudev <16504129+sagudev@users.noreply.github.com>
2022-08-11 17:08:43 +0200

    Make test_save_id3v2 more analogous to taglib test

--- tests/taglib/test_aiff.rs
+++ tests/taglib/test_aiff.rs
@@ -18,6 +18,7 @@ fn test_aiff_properties() {
 	assert_eq!(properties.sample_rate(), Some(44100));
 	assert_eq!(properties.channels(), Some(1));
 	assert_eq!(properties.bit_depth(), Some(16));
+	// TODO: get those options in lofty
 	// CPPUNIT_ASSERT_EQUAL(2941U, f.audioProperties()->sampleFrames());
 	// CPPUNIT_ASSERT_EQUAL(false, f.audioProperties()->isAiffC());
 }
@@ -36,6 +37,7 @@ fn test_aifc_properties() {
 	assert_eq!(properties.sample_rate(), Some(44100));
 	assert_eq!(properties.channels(), Some(1));
 	assert_eq!(properties.bit_depth(), Some(16));
+	// TODO: get those options in lofty
 	// CPPUNIT_ASSERT_EQUAL(1622U, f.audioProperties()->sampleFrames());
 	// CPPUNIT_ASSERT_EQUAL(true, f.audioProperties()->isAiffC());
 	// CPPUNIT_ASSERT_EQUAL(ByteVector("ALAW"), f.audioProperties()->compressionType());
@@ -59,6 +61,7 @@ fn test_save_id3v2() {
 		tfile.insert_tag(tag);
 		file.rewind().unwrap();
 		tfile.save_to(&mut file).unwrap();
+		assert!(tfile.contains_tag_type(lofty::TagType::ID3v2));
 	}
 
 	file.rewind().unwrap();
@@ -74,6 +77,7 @@ fn test_save_id3v2() {
 		tfile.insert_tag(tag);
 		file.rewind().unwrap();
 		tfile.save_to(&mut file).unwrap();
+		assert!(!tfile.contains_tag_type(lofty::TagType::ID3v2));
 	}
 
 	file.rewind().unwrap();
@@ -83,9 +87,12 @@ fn test_save_id3v2() {
 
 		assert_eq!(tfile.file_type(), FileType::AIFF);
 
-		assert!(tfile.tag(lofty::TagType::ID3v2).is_none());
+		assert!(!tfile.contains_tag_type(lofty::TagType::ID3v2));
 	}
 }
+
+// TODO: testSaveID3v23
+// TODO: testDuplicateID3v2
 
 #[test]
 #[ignore]
