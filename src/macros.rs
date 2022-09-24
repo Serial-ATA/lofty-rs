911471a6b6e29e180c09d2c638760e0650f9e06a
Serial <69764315+Serial-ATA@users.noreply.github.com>
2022-09-24 02:16:51 -0400

    Macros: Document `parse_mode_choice!`

--- src/macros.rs
+++ src/macros.rs
@@ -68,7 +68,20 @@ macro_rules! decode_err {
 	};
 }
 
-// TODO: document
+// A macro for handling the different `ParsingMode`s
+//
+// NOTE: All fields are optional, if `STRICT` or `RELAXED` are missing, it will
+// 		 fall through to `DEFAULT`. If `DEFAULT` is missing, it will fall through
+// 		 to an empty block.
+//
+// Usage:
+//
+// - parse_mode_choice!(
+// 		ident_of_parsing_mode,
+// 		STRICT: some_expr,
+// 		RELAXED: some_expr,
+// 		DEFAULT: some_expr,
+// 	 )
 macro_rules! parse_mode_choice {
 	(
 		$parse_mode:ident,
