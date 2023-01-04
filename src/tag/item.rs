d2d95d86ab109d3be01493dba92d26a98dd6d0f9
Uwe Klotz <uwe.klotz@gmail.com>
2023-01-04 17:34:10 +0100

    vorbis: Add Label variant as recommended by MusicBrainz

--- src/tag/item.rs
+++ src/tag/item.rs
@@ -318,7 +318,7 @@ gen_map!(
 	"PERFORMER"                    => Performer,
 	"PRODUCER"                     => Producer,
 	"PUBLISHER"                    => Publisher,
-	"LABEL"                        => Label,
+	"LABEL" | "ORGANIZATION"       => Label,
 	"REMIXER"                      => Remixer,
 	"DISCNUMBER"                   => DiscNumber,
 	"DISCTOTAL" | "TOTALDISCS"     => DiscTotal,
