06b3b6dbafe382fc836435ed7c8e523b57c5b5c8
Uwe Klotz <uwe.klotz@gmail.com>
2023-01-14 21:42:50 +0100

    mp4: More robust parsing and updated mapping of `rtng` atom

--- src/mp4/ilst/atom.rs
+++ src/mp4/ilst/atom.rs
@@ -216,14 +216,22 @@ pub enum AtomData {
 }
 
 /// The parental advisory rating
+///
+/// See also:
+/// <https://docs.mp3tag.de/mapping/#itunesadvisory>
+/// <https://exiftool.org/TagNames/QuickTime.html>
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum AdvisoryRating {
-	/// A rating of 0
+	/// *Inoffensive*/*None* (0)
 	Inoffensive,
-	/// A rating of 2
-	Clean,
-	/// A rating of (1 || > 2)
+	/// *Explicit* (1 or 4)
+	///
+	/// In the past Apple used the value 4 for explicit content
+	/// that has later been replaced by 1. Both values are considered
+	/// as valid when reading but only the newer value 1 is written.
 	Explicit,
+	/// *Clean*/*Edited* (2)
+	Clean,
 }
 
 impl AdvisoryRating {
@@ -231,18 +239,21 @@ impl AdvisoryRating {
 	pub fn as_u8(&self) -> u8 {
 		match self {
 			AdvisoryRating::Inoffensive => 0,
+			AdvisoryRating::Explicit => 1,
 			AdvisoryRating::Clean => 2,
-			AdvisoryRating::Explicit => 4,
 		}
 	}
 }
 
-impl From<u8> for AdvisoryRating {
-	fn from(input: u8) -> Self {
+impl TryFrom<u8> for AdvisoryRating {
+	type Error = u8;
+
+	fn try_from(input: u8) -> Result<Self, Self::Error> {
 		match input {
-			0 => Self::Inoffensive,
-			2 => Self::Clean,
-			_ => Self::Explicit,
+			0 => Ok(Self::Inoffensive),
+			1 | 4 => Ok(Self::Explicit),
+			2 => Ok(Self::Clean),
+			value => Err(value),
 		}
 	}
 }
