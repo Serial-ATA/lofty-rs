99af322afb2916d81b086fde309bbd55071ecaff
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-04-11 20:08:17 -0400

    fuzz: Remove picture_from_apic_bytes target

--- fuzz/fuzz_targets/picture_from_apic_bytes.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use lofty::id3::v2::ID3v2Version;
-
-fuzz_target!(|data: &[u8]| {
-	let _ = lofty::Picture::from_apic_bytes(data, ID3v2Version::V4);
-});
