d4b10fcc060778afa2aa7ee6d9275605eaf2f30f
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-04-23 14:40:21 -0400

    fuzz: Fix type names in `{aacfile,mpegfile}_read_from` targets

--- fuzz/fuzz_targets/aacfile_read_from.rs
+++ fuzz/fuzz_targets/aacfile_read_from.rs
@@ -6,7 +6,7 @@ use libfuzzer_sys::fuzz_target;
 use lofty::{AudioFile, ParseOptions};
 
 fuzz_target!(|data: Vec<u8>| {
-	let _ = lofty::aac::AACFile::read_from(
+	let _ = lofty::aac::AacFile::read_from(
 		&mut Cursor::new(data),
 		ParseOptions::new().read_properties(false),
 	);
