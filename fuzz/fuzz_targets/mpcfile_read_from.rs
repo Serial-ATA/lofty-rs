a311a888cacda8238a5b6ff948931b95bc0ee34b
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-05-23 18:46:24 -0400

    musepack: Add fuzz and bench targets

---
+++ fuzz/fuzz_targets/mpcfile_read_from.rs
@@ -0,0 +1,10 @@
+#![no_main]
+
+use std::io::Cursor;
+
+use libfuzzer_sys::fuzz_target;
+use lofty::{AudioFile, ParseOptions};
+
+fuzz_target!(|data: Vec<u8>| {
+	let _ = lofty::musepack::MpcFile::read_from(&mut Cursor::new(data), ParseOptions::new());
+});
