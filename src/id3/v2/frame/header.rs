d9c668f1e3440ffa2da9c005a2b948f1d58dba84
Uwe Klotz <uwe.klotz@gmail.com>
2023-07-03 10:10:11 +0200

    id3v2: Don't stop reading after skipping a frame

--- src/id3/v2/frame/header.rs
+++ src/id3/v2/frame/header.rs
@@ -3,95 +3,73 @@ use crate::error::{Id3v2Error, Id3v2ErrorKind, Result};
 use crate::id3::v2::util::synchsafe::SynchsafeInteger;
 use crate::id3::v2::util::upgrade::{upgrade_v2, upgrade_v3};
 use crate::id3::v2::FrameId;
-use crate::ParsingMode;
 
 use std::borrow::Cow;
 use std::io::Read;
 
 pub(crate) fn parse_v2_header<R>(
 	reader: &mut R,
-	parse_mode: ParsingMode,
 ) -> Result<Option<(FrameId<'static>, u32, FrameFlags)>>
 where
 	R: Read,
 {
-	let mut frame_header = [0; 6];
-	match reader.read_exact(&mut frame_header) {
+	let mut header = [0; 6];
+	match reader.read_exact(&mut header) {
 		Ok(_) => {},
 		Err(_) => return Ok(None),
 	}
 
 	// Assume we just started reading padding
-	if frame_header[0] == 0 {
+	if header[0] == 0 {
 		return Ok(None);
 	}
 
-	let frame_id_bytes = &frame_header[..3];
-	let frame_id = match std::str::from_utf8(frame_id_bytes)
-		.map_err(|_| Id3v2Error::new(Id3v2ErrorKind::BadFrameId(frame_id_bytes.to_vec())).into())
+	let id_bytes = &header[..3];
+	let id_str = std::str::from_utf8(id_bytes)
+		.map_err(|_| Id3v2Error::new(Id3v2ErrorKind::BadFrameId(id_bytes.to_vec())))
 		.map(|id_str| {
 			upgrade_v2(id_str).map_or_else(|| Cow::Owned(id_str.to_owned()), Cow::Borrowed)
-		})
-		.and_then(FrameId::new_cow)
-	{
-		Ok(id) => id,
-		Err(err) => match parse_mode {
-			ParsingMode::Strict => return Err(err),
-			ParsingMode::BestAttempt | ParsingMode::Relaxed => return Ok(None),
-		},
-	};
+		})?;
+	let id = FrameId::new_cow(id_str)?;
 
-	let size = u32::from_be_bytes([0, frame_header[3], frame_header[4], frame_header[5]]);
+	let size = u32::from_be_bytes([0, header[3], header[4], header[5]]);
 
 	// V2 doesn't store flags
-	Ok(Some((frame_id, size, FrameFlags::default())))
+	Ok(Some((id, size, FrameFlags::default())))
 }
 
 pub(crate) fn parse_header<R>(
 	reader: &mut R,
 	synchsafe: bool,
-	parse_mode: ParsingMode,
 ) -> Result<Option<(FrameId<'static>, u32, FrameFlags)>>
 where
 	R: Read,
 {
-	let mut frame_header = [0; 10];
-	match reader.read_exact(&mut frame_header) {
+	let mut header = [0; 10];
+	match reader.read_exact(&mut header) {
 		Ok(_) => {},
 		Err(_) => return Ok(None),
 	}
 
 	// Assume we just started reading padding
-	if frame_header[0] == 0 {
+	if header[0] == 0 {
 		return Ok(None);
 	}
 
 	// For some reason, some apps make v3 tags with v2 frame IDs.
 	// The actual frame header is v3 though
-	let mut frame_id_end = 4;
+	let mut id_end = 4;
 	let mut invalid_v2_frame = false;
-	if frame_header[3] == 0 && !synchsafe {
+	if header[3] == 0 && !synchsafe {
 		invalid_v2_frame = true;
-		frame_id_end = 3;
+		id_end = 3;
 	}
 
-	let frame_id_bytes = &frame_header[..frame_id_end];
-	let id_str = match std::str::from_utf8(frame_id_bytes)
-		.map_err(|_| Id3v2Error::new(Id3v2ErrorKind::BadFrameId(frame_id_bytes.to_vec())).into())
-	{
-		Ok(id_str) => id_str,
-		Err(err) => match parse_mode {
-			ParsingMode::Strict => return Err(err),
-			ParsingMode::BestAttempt | ParsingMode::Relaxed => return Ok(None),
-		},
-	};
+	let id_bytes = &header[..id_end];
+	let id_str = std::str::from_utf8(id_bytes)
+		.map_err(|_| Id3v2Error::new(Id3v2ErrorKind::BadFrameId(id_bytes.to_vec())))?;
 
-	let mut size = u32::from_be_bytes([
-		frame_header[4],
-		frame_header[5],
-		frame_header[6],
-		frame_header[7],
-	]);
+	let mut size = u32::from_be_bytes([header[4], header[5], header[6], header[7]]);
 
 	// Now upgrade the FrameId
 	let id = if invalid_v2_frame {
@@ -105,20 +83,14 @@ where
 	} else {
 		Cow::Owned(id_str.to_owned())
 	};
-	let frame_id = match FrameId::new_cow(id) {
-		Ok(frame_id) => frame_id,
-		Err(err) => match parse_mode {
-			ParsingMode::Strict => return Err(err),
-			ParsingMode::BestAttempt | ParsingMode::Relaxed => return Ok(None),
-		},
-	};
+	let frame_id = FrameId::new_cow(id)?;
 
 	// unsynch the frame size if necessary
 	if synchsafe {
 		size = size.unsynch();
 	}
 
-	let flags = u16::from_be_bytes([frame_header[8], frame_header[9]]);
+	let flags = u16::from_be_bytes([header[8], header[9]]);
 	let flags = parse_flags(flags, synchsafe);
 
 	Ok(Some((frame_id, size, flags)))
