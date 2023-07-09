bc25e7315aefae5998e7990567103425160115a9
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-07-08 23:28:14 -0400

    Tests: Add TagLib SynchData tests

---
+++ tests/taglib/test_synchdata.rs
@@ -0,0 +1,90 @@
+use lofty::id3::v2::util::synchsafe::{SynchsafeInteger, UnsynchronizedStream};
+use std::io::Read;
+
+#[test]
+fn test1() {
+	let v = u32::from_be_bytes([0, 0, 0, 127]);
+
+	assert_eq!(v.unsynch(), 127);
+	assert_eq!(127u32.synch().unwrap(), v);
+}
+
+#[test]
+fn test2() {
+	let v = u32::from_be_bytes([0, 0, 1, 0]);
+
+	assert_eq!(v.unsynch(), 128);
+	assert_eq!(128u32.synch().unwrap(), v);
+}
+
+#[test]
+fn test3() {
+	let v = u32::from_be_bytes([0, 0, 1, 1]);
+
+	assert_eq!(v.unsynch(), 129);
+	assert_eq!(129u32.synch().unwrap(), v);
+}
+
+#[test]
+#[ignore]
+fn test_to_uint_broken() {
+	// Marker test, this behavior is not replicated in Lofty
+}
+
+#[test]
+#[ignore]
+fn test_to_uint_broken_and_too_large() {
+	// Marker test, this behavior is not replicated in Lofty
+}
+
+#[test]
+fn test_decode1() {
+	let a = [0xFFu8, 0x00u8, 0x00u8];
+
+	let mut a2 = Vec::new();
+	UnsynchronizedStream::new(&mut &a[..])
+		.read_to_end(&mut a2)
+		.unwrap();
+
+	assert_eq!(a2.len(), 2);
+	assert_eq!(a2, &[0xFF, 0x00]);
+}
+
+#[test]
+fn test_decode2() {
+	let a = [0xFFu8, 0x44u8];
+
+	let mut a2 = Vec::new();
+	UnsynchronizedStream::new(&mut &a[..])
+		.read_to_end(&mut a2)
+		.unwrap();
+
+	assert_eq!(a2.len(), 2);
+	assert_eq!(a2, &[0xFF, 0x44]);
+}
+
+#[test]
+fn test_decode3() {
+	let a = [0xFFu8, 0xFFu8, 0x00u8];
+
+	let mut a2 = Vec::new();
+	UnsynchronizedStream::new(&mut &a[..])
+		.read_to_end(&mut a2)
+		.unwrap();
+
+	assert_eq!(a2.len(), 2);
+	assert_eq!(a2, &[0xFFu8, 0xFFu8]);
+}
+
+#[test]
+fn test_decode4() {
+	let a = [0xFFu8, 0xFFu8, 0xFFu8];
+
+	let mut a2 = Vec::new();
+	UnsynchronizedStream::new(&mut &a[..])
+		.read_to_end(&mut a2)
+		.unwrap();
+
+	assert_eq!(a2.len(), 3);
+	assert_eq!(a2, &[0xFFu8, 0xFFu8, 0xFFu8]);
+}
