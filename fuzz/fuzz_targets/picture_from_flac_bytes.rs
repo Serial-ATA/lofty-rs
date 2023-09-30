aea857cec2663edbf72df06c75d3284df7ffff17
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-09-30 12:53:21 -0400

    Tests/Fuzz: Fix arguments to `Picture::from_flac_bytes`

--- fuzz/fuzz_targets/picture_from_flac_bytes.rs
+++ fuzz/fuzz_targets/picture_from_flac_bytes.rs
@@ -1,7 +1,9 @@
 #![no_main]
+use lofty::ParsingMode;
+
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = lofty::Picture::from_flac_bytes(data, true);
-    let _ = lofty::Picture::from_flac_bytes(data, false);
+	let _ = lofty::Picture::from_flac_bytes(data, true, ParsingMode::Relaxed);
+	let _ = lofty::Picture::from_flac_bytes(data, false, ParsingMode::Relaxed);
 });
