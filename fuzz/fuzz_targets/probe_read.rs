4a8fc92be026e9fc781996e2407805c950a3c076
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-10-22 23:39:11 -0400

    Fuzz: Add target for `Probe::read`

---
+++ fuzz/fuzz_targets/probe_read.rs
@@ -0,0 +1,12 @@
+#![no_main]
+
+use std::io::Cursor;
+
+use libfuzzer_sys::fuzz_target;
+use lofty::Probe;
+
+fuzz_target!(|data: Vec<u8>| {
+	if let Ok(probe) = Probe::new(&mut Cursor::new(data)).guess_file_type() {
+		let _ = probe.read();
+	}
+});
