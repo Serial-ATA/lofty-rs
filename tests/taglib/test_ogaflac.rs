991f3648385277bb91ed56ec1faa84cd54194cc3
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-12-03 15:21:38 -0500

    Tests: Ignore more TagLib tests with unimplemented features

--- tests/taglib/test_ogaflac.rs
+++ tests/taglib/test_ogaflac.rs
@@ -6,30 +6,31 @@ use lofty::flac::FlacFile;
 use lofty::ogg::VorbisComments;
 use lofty::{Accessor, AudioFile, ParseOptions};
 
+// TODO: We don't support FLAC in OGA (#172)
 #[test]
+#[ignore]
 fn test_framing_bit() {
-	todo!("We don't support FLAC in OGA")
-	// let mut file = temp_file!("tests/taglib/data/empty_flac.oga");
-	//
-	// {
-	// 	let mut f = FlacFile::read_from(&mut file, ParseOptions::new()).unwrap();
-	// 	file.rewind().unwrap();
-	//
-	// 	let mut vorbis_comments = VorbisComments::new();
-	// 	vorbis_comments.set_artist(String::from("The Artist"));
-	// 	f.set_vorbis_comments(vorbis_comments);
-	// 	f.save_to(&mut file).unwrap();
-	// }
-	// file.rewind().unwrap();
-	// {
-	// 	let f = FlacFile::read_from(&mut file, ParseOptions::new()).unwrap();
-	// 	assert_eq!(
-	// 		f.vorbis_comments().unwrap().artist().as_deref(),
-	// 		Some("The Artist")
-	// 	);
-	//
-	// 	assert_eq!(file.seek(SeekFrom::End(0)).unwrap(), 9134);
-	// }
+	let mut file = temp_file!("tests/taglib/data/empty_flac.oga");
+
+	{
+		let mut f = FlacFile::read_from(&mut file, ParseOptions::new()).unwrap();
+		file.rewind().unwrap();
+
+		let mut vorbis_comments = VorbisComments::new();
+		vorbis_comments.set_artist(String::from("The Artist"));
+		f.set_vorbis_comments(vorbis_comments);
+		f.save_to(&mut file).unwrap();
+	}
+	file.rewind().unwrap();
+	{
+		let f = FlacFile::read_from(&mut file, ParseOptions::new()).unwrap();
+		assert_eq!(
+			f.vorbis_comments().unwrap().artist().as_deref(),
+			Some("The Artist")
+		);
+
+		assert_eq!(file.seek(SeekFrom::End(0)).unwrap(), 9134);
+	}
 }
 
 // TODO: We don't support FLAC in OGA (#172)
