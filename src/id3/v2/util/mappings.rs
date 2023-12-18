07ddca376212ebf59a65ffc685ce48c718404748
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-12-17 19:24:14 -0500

    ID3v2: Support building TIPL frames on `Tag` merge

---
+++ src/id3/v2/util/mappings.rs
@@ -0,0 +1,9 @@
+use crate::tag::item::ItemKey;
+
+pub(crate) const TIPL_MAPPINGS: &[(ItemKey, &str)] = &[
+	(ItemKey::Producer, "producer"),
+	(ItemKey::Arranger, "arranger"),
+	(ItemKey::Engineer, "engineer"),
+	(ItemKey::MixDj, "DJ-mix"),
+	(ItemKey::MixEngineer, "mix"),
+];
--- src/id3/v2/util/mod.rs
+++ src/id3/v2/util/mod.rs
@@ -1,5 +1,6 @@
 //! Utilities for working with ID3v2 tags
 
+pub(crate) mod mappings;
 pub(crate) mod pairs;
 pub mod synchsafe;
 pub mod upgrade;
