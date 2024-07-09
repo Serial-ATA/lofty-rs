60bd996e72534660df0c4a7c9f7204dbbdfc15ae
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-07-09 19:51:06 -0400

    Vorbis: Fix multiply with overflow panic

--- lofty/tests/fuzz/vorbisfile_read_from.rs
+++ lofty/tests/fuzz/vorbisfile_read_from.rs
@@ -1,7 +1,16 @@
 use crate::oom_test;
+use lofty::config::ParseOptions;
+use lofty::file::AudioFile;
 use lofty::ogg::VorbisFile;
 
 #[test]
 fn oom1() {
 	oom_test::<VorbisFile>("vorbisfile_read_from/oom-436193bc2d1664b74c19720bef08697d03284f06");
+}
+
+#[test]
+fn panic1() {
+	let mut reader =
+		crate::get_reader("vorbisfile_read_from/order01d_IDX_32_RAND_22064097693866277502540.ogg");
+	let _ = VorbisFile::read_from(&mut reader, ParseOptions::new());
 }
