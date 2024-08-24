34238b5ec113a2d8534cee24d4b9cdb17721d7bb
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-08-24 12:45:06 -0400

    AAC: Fix overflow on tag size mismatches

---
+++ lofty/tests/fuzz/aacfile_read_from.rs
@@ -0,0 +1,11 @@
+use lofty::aac::AacFile;
+use lofty::config::ParseOptions;
+use lofty::file::AudioFile;
+
+#[test]
+fn panic1() {
+	let mut reader = crate::get_reader(
+		"aacfile_read_from/01 -  aalborg_IDX_9_RAND_168952727934877251846138.mp3",
+	);
+	let _ = AacFile::read_from(&mut reader, ParseOptions::new());
+}
