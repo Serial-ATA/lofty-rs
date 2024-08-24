3701bb145b14df50b191d1019458e08aa48b0e0a
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-08-24 11:33:39 -0400

    EBML: Add basic matroska integration tests (not yet working)

---
+++ lofty/tests/files/matroska.rs
@@ -0,0 +1,67 @@
+use crate::{set_artist, temp_file, verify_artist};
+use lofty::config::ParseOptions;
+use lofty::file::FileType;
+use lofty::prelude::*;
+use lofty::probe::Probe;
+use lofty::tag::TagType;
+
+use std::io::Seek;
+
+#[test]
+fn read() {
+	// This file contains a tags element
+	let file = Probe::open("tests/files/assets/minimal/full_test.mka")
+		.unwrap()
+		.options(ParseOptions::new().read_properties(false))
+		.read()
+		.unwrap();
+
+	assert_eq!(file.file_type(), FileType::Ebml);
+
+	// Verify the tag
+	crate::verify_artist!(file, primary_tag, "Foo artist", 1);
+}
+
+#[test]
+fn write() {
+	let mut file = temp_file!("tests/files/assets/minimal/full_test.mka");
+
+	let mut tagged_file = Probe::new(&mut file)
+		.options(ParseOptions::new().read_properties(false))
+		.guess_file_type()
+		.unwrap()
+		.read()
+		.unwrap();
+
+	assert_eq!(tagged_file.file_type(), FileType::Ebml);
+
+	// Tags
+	crate::set_artist!(tagged_file, tag_mut, TagType::Ebml, "Foo artist", 1 => file, "Bar artist");
+
+	// Now reread the file
+	file.rewind().unwrap();
+
+	let mut tagged_file = Probe::new(&mut file)
+		.options(ParseOptions::new().read_properties(false))
+		.guess_file_type()
+		.unwrap()
+		.read()
+		.unwrap();
+
+	crate::set_artist!(tagged_file, tag_mut, TagType::Ebml, "Bar artist", 1 => file, "Foo artist");
+}
+
+#[test]
+fn remove() {
+	crate::remove_tag!("tests/files/assets/minimal/full_test.mka", TagType::Ebml);
+}
+
+#[test]
+fn read_no_properties() {
+	crate::no_properties_test!("tests/files/assets/minimal/full_test.mka");
+}
+
+#[test]
+fn read_no_tags() {
+	crate::no_tag_test!("tests/files/assets/minimal/full_test.mka");
+}
