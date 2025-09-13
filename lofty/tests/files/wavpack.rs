62852cb6b5c9e16b8079490b95e7c269b3b341d7
Ayase <166034664+AyaseFile@users.noreply.github.com>
2025-09-14 02:27:42 +0800

    fix: incorrect disc number handling in ape tag writer

--- lofty/tests/files/wavpack.rs
+++ lofty/tests/files/wavpack.rs
@@ -1,9 +1,10 @@
 use crate::{set_artist, temp_file, verify_artist};
-use lofty::config::ParseOptions;
+use lofty::ape::ApeTag;
+use lofty::config::{ParseOptions, WriteOptions};
 use lofty::file::FileType;
 use lofty::prelude::*;
 use lofty::probe::Probe;
-use lofty::tag::TagType;
+use lofty::tag::{Tag, TagType};
 
 use std::io::Seek;
 
@@ -76,4 +77,43 @@ fn read_no_properties() {
 #[test_log::test]
 fn read_no_tags() {
 	crate::no_tag_test!("tests/files/assets/minimal/full_test.wv");
+}
+
+#[test_log::test]
+fn write_ape_disc_key() {
+	let mut file = crate::temp_file!("tests/files/assets/minimal/full_test.wv");
+	let mut tagged_file = Probe::new(&mut file)
+		.options(ParseOptions::new())
+		.guess_file_type()
+		.unwrap()
+		.read()
+		.unwrap();
+
+	// Create and insert a new Tag and set disk information
+	let mut tag = Tag::new(TagType::Ape);
+	tag.set_disk(3);
+	tag.set_disk_total(5);
+	tagged_file.insert_tag(tag);
+	file.rewind().unwrap();
+	tagged_file
+		.save_to(&mut file, WriteOptions::default())
+		.unwrap();
+
+	// Reread the file to get the actual APE tag
+	file.rewind().unwrap();
+	let reread_tagged_file = Probe::new(&mut file)
+		.options(ParseOptions::new())
+		.guess_file_type()
+		.unwrap()
+		.read()
+		.unwrap();
+	let tag_ref = reread_tagged_file.tag(TagType::Ape).unwrap();
+	let ape_tag: ApeTag = tag_ref.clone().into();
+
+	assert!(
+		ape_tag.get("Disc").is_some(),
+		"APE tag should contain `Disc` key with disk information"
+	);
+	assert_eq!(ape_tag.disk(), Some(3));
+	assert_eq!(ape_tag.disk_total(), Some(5));
 }
